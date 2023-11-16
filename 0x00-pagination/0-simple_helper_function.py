bcaa7837637114bfc9500edd11855b3f44197cdc
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-11-16 17:24:05 +0100

    pagination task 1

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Pagination
+"""
+
+def index_range(page: int, page_size: int) -> tuple:
+    """ Get index"""
+    start_index = (page_size * page) - page_size
+    end_index = page_size * page
+    return start_index, end_index
