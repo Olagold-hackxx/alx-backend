d17a537d9093c8038af4454d3aab04094c6cd1fc
Doyinsola Aladeloye <doyinsolaaladeloye@gmail.com>
2023-11-24 03:23:42 +0100

    task 6

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -41,8 +41,11 @@ def before_request():
 @babel.localeselector
 def get_locale():
     """Get the locale"""
-    if g.user:
-        return g.user['locale']
+    requested_locale = request.args.get('locale')
+
+    # If the requested locale is supported, return it
+    if requested_locale and requested_locale in app.config['LANGUAGES']:
+        return requested_locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
