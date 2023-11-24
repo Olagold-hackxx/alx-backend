274abd62ea903bcc04330f6a454cd97c7d6d6b38
Doyinsola Aladeloye <doyinsolaaladeloye@gmail.com>
2023-11-24 03:25:01 +0100

    task 6

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Flask babel"""
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 app = Flask(__name__)
